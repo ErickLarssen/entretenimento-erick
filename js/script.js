9385b424817b8d12472f767d8107b4427a8d2239
Erick <122824991+ErickLarssen@users.noreply.github.com>
2025-03-27 17:21:59 -0300

    Terceiro commit
    Estrutura do código pronta, só falta atualizar as informações!

--- js/script.js
+++ js/script.js
@@ -11,4 +11,26 @@ function changeBg(bg, title){
             content.classList.add('active');
         }
     })
+
+    // Se estiver no primeiro slide, aplica a imagem padrão
+    if (title === 'home') {
+        banner.style.background = `url("../images/erick¹.png") no-repeat`;
+        banner.style.backgroundSize = '850px';
+        banner.style.backgroundPosition = 'center';
+    } else {
+        // Caso contrário, muda para o fundo da série específica
+        banner.style.background = `url("../images/series/${bg}")`;
+        banner.style.backgroundSize = 'cover';
+        banner.style.backgroundPosition = 'center';
+    }
+
+    // Alterando o conteúdo ativo
+    contents.forEach(content => {
+        content.classList.remove('active');
+        if(content.classList.contains(title)){
+            content.classList.add('active');
+        }
+    });
 }
+
+
